c8ff098fcbc3f9c190a621c9f6cf0370d96e569b
Y4Ru <marcel.bechtold@web.de>
2015-07-31 01:21:27 +0200

    fullShuffle() now returns an array of objects
    The objects are specified in the array stim_items in shuffle.js

--- shuffle/test/testShuffle.js
+++ shuffle/test/testShuffle.js
@@ -1,13 +1,14 @@
 function test()
 {
+	//var randomized = fullShuffle();
+	//appendParagraph(randomized[0].learning + " " + randomized[0].family + " " + randomized[0].alien + " " + randomized[0].path);
 	var validCount = 0;
 	var invalidCount = 0;
 	var n = document.getElementById("trials").value;
-	
+		
 	for(var i=0; i<n; i++)
 	{
-		var order = [];
-		order = fullShuffle();	
+		var order = fullShuffle();	
 		if(hasArraySameSuccessor(order))
 		{
 			invalidCount += 1;
@@ -15,12 +16,22 @@ function test()
 		{
 			validCount += 1;
 		}
-		appendParagraph("Elements in order: " + order.length + " order: " + order);
+
+		appendParagraph("Elements in order: " + order.length);
+		printObjectOrder(order);
 	}
 	appendParagraph("Test Result:");
 	appendParagraph("Valid trials: " + validCount + "/" + n);
 	appendParagraph("Invalid trials: " + invalidCount + "/" + n);
 	
+}
+
+function printObjectOrder(order)
+{
+	for(var i=0; i<order.length; i++)
+	{
+		appendParagraph(order[i].family + " // " + order[i].learning + " // " + order[i].alien + " //" + order[i].path);
+	}
 }
 
 //checks whether two successive array elements have the same value 
